7d19a4c1707784229f0b48b1ad3ddd154670e01f
Sceris <91082239+Scer1s@users.noreply.github.com>
2023-03-10 08:55:17 -0600

    Update bot.py

--- Botcode/bot.py
+++ Botcode/bot.py
@@ -1,1 +1,20 @@
+import discord
+from discord.ext import commands, tasks
+import random
+from itertools import cycle
+import sqlite3
+import sys
+import datetime
+import asyncio
+import pprint
+import time
 
+client = commands.Bot(command_prefix = '.')  #Bot command prefix such as .kick or .command
+
+@client.event
+async def on_ready():
+	change_status.start()
+	print('Bot is ready.')
+
+  
+  client.run('NjM1ODc3NjcxNzgzNjI4ODE0.Xa3dOg.iwsKYUnQZNFsugzVlG2-fBKuYy8') #Botkey
