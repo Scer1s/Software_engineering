23acd03a8ef6e3d2800f273535ddc038f5786890
Sceris <91082239+Scer1s@users.noreply.github.com>
2023-03-29 13:51:30 -0500

    Shell botcode

--- Botcode/bot.py
+++ Botcode/bot.py
@@ -1,18 +1,46 @@
 import discord
-from discord.ext import commands, tasks
-from itertools import cycle
-import sys
-import asyncio
-import pprint
+import responses
 
 
-client = commands.Bot(command_prefix = '.')  #Bot command prefix such as .kick or .command
 
-@client.event
-async def on_ready():
-	change_status.start()
-	print('Bot is ready.')
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        if response == 'Undef resp':
+            pass
+        else:
+            await message.author.send(response) if is_private else await message.channel.send(response) #sends either to the channel or to the user
+        
+    except Exception as e:
+        pass
 
-#CODE GOES HERE
-  
-  client.run('NjM1ODc3NjcxNzgzNjI4ODE0.Xa3dOg.iwsKYUnQZNFsugzVlG2-fBKuYy8') #Botkey (replace with secret botkey)
+
+def run_discord_bot(): #DELETE TOKEN BEFORE PUSHING TO GITHUB AND PUT IT BACK IN BEFORE TESTING
+    TOKEN = 'MTA5MDY5Mzc1OTU1NTA5MjYxMA.GbBhZJ.xDMIO31_BN2IPOUBrA7nByvB91aH92smc14kPM' #DO NOT POST THIS ANYWHERE
+    intents = discord.Intents.all() #intents are new security features in discord, ask sceris before fucking with this
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event #handles startup sequence
+    async def on_ready():
+        print(f'{client.user} is locked and loaded B)')
+
+    @client.event #handles messages
+    async def on_message(message):
+        if message.author == client.user: #stops the bot from looping on its own messages
+            return
+
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said:"{user_message}" in:({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
