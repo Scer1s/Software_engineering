f3a963404aa3b5d8ebdd0f041287f4ee860a2fc5
Sceris <91082239+Scer1s@users.noreply.github.com>
2023-03-29 13:54:08 -0500

    uploaded necessary files

---
+++ Botcode/responses.py
@@ -0,0 +1,15 @@
+import random
+
+def get_response(message: str ) -> str: #gets the response from defined code
+    p_message = message.lower()
+
+    if p_message == 'hello':
+        return 'Sup'
+
+    if message == 'roll':
+        return str(random.randint(1,6))
+
+    if p_message == '!help':
+        return 'Help message deployed'
+
+    return
