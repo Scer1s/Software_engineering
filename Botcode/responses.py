b5a0d071a9d3d62d919207cf5eaa1b9826eea8b5
OneTr1ckNick <70983861+OneTr1ckNick@users.noreply.github.com>
2023-03-30 16:34:27 -0500

    Added "whoami" command

--- Botcode/responses.py
+++ Botcode/responses.py
@@ -1,6 +1,7 @@
 import random
+import discord
 
-def get_response(message: str ) -> str: #gets the response from defined code
+def get_response(ctx, message): #gets the response from defined code
     p_message = message.lower()
 
     if p_message == 'hello':
@@ -11,5 +12,9 @@ def get_response(message: str ) -> str: #gets the response from defined code
 
     if p_message == '!help':
         return 'Help message deployed'
+
+    if p_message == 'whoami':
+        result = (f'You are {ctx.author.mention}. \nYou joined Discord on {ctx.author.created_at.strftime("%a, %b %#d, %Y")}\nYou joined this server on {ctx.author.joined_at.strftime("%a, %b %#d, %Y")}')
+        return result
 
     return
