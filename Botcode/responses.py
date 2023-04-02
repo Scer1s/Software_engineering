04c99f25f0c4e042370fed098d88fe323d710bb7
Turbo1025 <91082230+Turbo1025@users.noreply.github.com>
2023-04-02 10:09:30 -0500

    codehelp command (#8)
    * YouTube command
    
    * codehelp command
    
    ---------
    
    Co-authored-by: OneTr1ckNick <70983861+OneTr1ckNick@users.noreply.github.com>

--- Botcode/responses.py
+++ Botcode/responses.py
@@ -6,8 +6,14 @@ def get_response(message: str ) -> str: #gets the response from defined code
     if p_message == 'hello':
         return 'Sup'
 
-    if message == 'roll':
+    if p_message == 'roll':
         return str(random.randint(1,6))
+
+    if p_message == '-youtube':
+        return 'https://www.youtube.com/'
+
+    if p_message == '-codehelp':
+        return 'https://www.geeksforgeeks.org/', 'https://stackoverflow.com/', 'https://www.tutorialspoint.com/index.htm', 'https://www.w3schools.com/'
 
     if p_message == '!help':
         return 'Help message deployed'
