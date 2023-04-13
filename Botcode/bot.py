80e7721cd287a35f98e4df83a2f864aff0c3a155
Sceris <91082239+Scer1s@users.noreply.github.com>
2023-04-13 12:52:05 -0500

    MESSAGE DATABASE ADDED

--- Botcode/bot.py
+++ Botcode/bot.py
@@ -1,7 +1,7 @@
 import discord
 import responses
 import json
-
+import sqlite3
 
 async def send_message(message, user_message, is_private):
     try:
@@ -15,18 +15,20 @@ async def send_message(message, user_message, is_private):
         #print(e) for debugging
         pass
 
-
 def run_discord_bot(): #DELETE TOKEN BEFORE PUSHING TO GITHUB AND PUT IT BACK IN BEFORE TESTING
     with open('config.json', 'r') as cfg:
         data = json.load(cfg)
     TOKEN = data["BOTTOKEN"]
+    print(TOKEN)
+    
     intents = discord.Intents.all() #intents are new security features in discord, ask sceris before fucking with this
     intents.message_content = True
     client = discord.Client(intents=intents)
 
-    @client.event #handles startup sequence
+    @client.event #handles startup sequence and connects it to a database
     async def on_ready():
         print(f'{client.user} is locked and loaded B)')
+
 
     @client.event #handles messages
     async def on_message(message):
@@ -44,6 +46,13 @@ def run_discord_bot(): #DELETE TOKEN BEFORE PUSHING TO GITHUB AND PUT IT BACK IN
             user_message = user_message[1:]
             await send_message(message, user_message, is_private=True)
         else:
+            db = sqlite3.connect('message.sqlite')
+            cursor = db.cursor()
+            cursor.execute("INSERT INTO Main (Channel, Name, Message) VALUES ( ?, ?, ?)",
+		(channel, username, user_message))
+            db.commit()
+            cursor.close()
+            db.close()
             await send_message(message, user_message, is_private=False)
 
     client.run(TOKEN)
